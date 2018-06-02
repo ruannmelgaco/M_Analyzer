92a7e6057551943b476a0d8bf44c11ab39dcb199
Ruann Melgaço <ruannmelgaco@outlook.com>
2018-06-02 18:09:04 +0000

    UPGRADE
    Adicionada validação do usuário Root;
    Adicionada opção de instalação do NMON;
    Ajustada a visualização dos menus;

--- m_analyzer.sh
+++ m_analyzer.sh
@@ -23,25 +23,35 @@ find $dsrch -perm $gperm
 
 #Função inicial
 main(){
-echo "+-----------------------+----------------------+-------------------+"
-echo "|=====Monitoramento=====|=====Configuração=====|=======Busca=======|"
-echo "+-----------------------+----------------------+-------------------+"
-echo "|1 =       CPU          |7 =    Threshold      |10 =  Usuários     |"
-echo "|2 =     Memória        |                      |11 = Permissões    |"
-echo "|3 = Memória Virtual    |                      |                   |"
-echo "|4 =      Disco         |                      |                   |"
-echo "|5 =      Rede          |                      |                   |"
-echo "|6 =      Sair          |                      |                   |"
-echo "+-----------------------+----------------------+-------------------+"
+#Verifica se o usuário está logado como root
+if [ "$(id -u)" != "0" ]; then
+echo "Você precisa ser usuário Root para executar esta aplicação"
+exit
+else
+echo "                     Usuário root autenticado!"
+fi
+
+echo "+-----------------------+------------------------+-------------------+"
+echo "|=====Monitoramento=====|======Configuração======|=======Busca=======|"
+echo "+-----------------------+------------------------+-------------------+"
+echo "|1 =       CPU          |7 =     Threshold       |10 =  Usuários     |"
+echo "|2 =     Memória        |8 =   Instalar NMON     |11 = Permissões    |"
+echo "|3 = Memória Virtual    |                        |                   |"
+echo "|4 =      Disco         |                        |                   |"
+echo "|5 =      Rede          |                        |                   |"
+echo "|6 =      Sair          |                        |                   |"
+echo "+-----------------------+------------------------+-------------------+"
 
 read -p "--> " escolha
 case $escolha in
-1) echo c | nmon;;
-2) echo m | nmon;;
-3) echo V | nmon;;
-4) echo d | nmon;;
-5) echo n | nmon;;
+1) echo c | nmon;; #Executa a função de análise de CPU do NMON
+2) echo m | nmon;; #Executa a função de análise de Memória do NMON
+3) echo V | nmon;; #Executa a função de análise de Memória Virtual do NMON
+4) echo d | nmon;; #Executa a função de análise de Disco do NMON
+5) echo n | nmon;; #Executa a função de análise de Rede do NMON
 6) clear; echo "Script finalizado"; sleep 2; exit;;
+7) echo "Função não implementada";;
+8) apt-get install nmon;;
 10) findusr;;
 11) chkperm;;
 *) clear; echo "Opção inválida"; sleep2; menu
